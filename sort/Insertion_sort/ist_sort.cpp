ff49e84c62cc8ea2cc96a242c155cf386962191a
qh120411 <quanghuyy0411@gmail.com>
2025-12-01 15:46:27 +0700

    New istsort

--- sort/Insertion_sort/ist_sort.cpp
+++ sort/Insertion_sort/ist_sort.cpp
@@ -3,26 +3,26 @@
 using namespace std;
 
 void insertion_sort( int a[], int n) {
-        int min;
-        for ( int i = 0 ; i < n ; i++ ) {
-            min = i;
-            for ( int j = i+1 ; j < n ; j++ ) {
-                if ( a[j] < a[min]) {
-                    min = j;
-                }
-            }
-            if ( min != i ) {
-                int tmp = a[i];
-                a[i] = a[min];
-                a[min] = tmp;
-            }
+    for ( int i = 0 ; i < n ; i++ ) {
+        int value = a[i];
+        int index = i;
+        while ( index > 0 && a[index-1] > value) {
+            a[index] = a[index-1] ;
+            index--;
         }
+        a[index] = value;
+    }
 }
 
 int main() {
-    int a[5] = {5, 3, 4, 8, 6};
-    insertion_sort(a, 5);
-    for ( int i = 0 ; i < 5 ; i++ ) {
+    int n;
+    cin >> n;
+    int a[n];
+    for ( int i = 0 ; i < n ; i++ ) {
+        cin >> a[i];
+    }
+    insertion_sort(a, n);
+    for ( int i = 0 ; i < n ; i++ ) {
         cout << a[i] << " ";
     }
 }
