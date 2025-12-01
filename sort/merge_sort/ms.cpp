6992fc48bad5594d5fd6dfb65ff303ffb24b8d79
qh120411 <quanghuyy0411@gmail.com>
2025-12-01 17:05:24 +0700

    update code

--- sort/merge_sort/ms.cpp
+++ sort/merge_sort/ms.cpp
@@ -2,57 +2,68 @@
 
 using namespace std;
 
-void merge ( int a[], int n, int l, int r) {
-    int n1 = n - l + 1 ;
-    int n2 = r - n;
+void merge( int a[], int l, int m, int r) {
+
+    int i, j , k;
+
+    int n1 = m-l+1;
+    int n2 = r - m; // ben phai lay tu m+1
     int L[n1], R[n2];
+    
     for ( int i = 0 ; i < n1 ; i++ ) {
-        L[i] = a[l + i];
+        L[i] = a[l+i]; //khoi dau tu left nen cu cong don i cho den khi i < n1 thi thoi
     }
+
     for ( int j = 0 ; j < n2 ; j++ ) {
-        R[j] = a[n + 1 + j];
+        R[j] = a[m+1+j];
     }
-    int i = 0, j = 0, k = l;
+
+    i = 0, j = 0, k = l;
     while ( i < n1 && j < n2 ) {
-        if ( L[i] <= R[j]) {
+        if ( L[i] <= R[j] ) {
             a[k] = L[i];
             i++;
-        } else {
+        }
+        else {
             a[k] = R[j];
             j++;
         }
         k++;
     }
+
     while ( i < n1 ) {
         a[k] = L[i];
         i++;
         k++;
     }
+    
     while ( j < n2 ) {
         a[k] = R[j];
         j++;
         k++;
     }
+    
 }
 
-void merge_sort( int a[], int l, int r) {
+void mergesort ( int a[], int l, int r) {
     if ( l < r ) {
-        int n = l + ( r - l ) / 2;
-        merge_sort( a, l, n);
-        merge_sort( a, n + 1, r);
-        merge( a, n, l, r);
+        int m = l + (r-l)/2; // tránh bị tràn số
+        mergesort(a,l,m);
+        mergesort(a,m+1,r);
+        merge(a,l,m,r);
     }
-}
-
-void print ( int a[], int n) {
-    for ( int i = 0 ; i < n ; i++ ) {
-        cout << a[i] << " ";
-    }
-    cout << endl;
 }
 
 int main() {
-    int a[7] = {38, 27, 43, 3, 9, 82, 10};
-    merge_sort( a, 0, 6);
-    print( a, 7);
-}                                                                                                                                                                                            
+    int n;
+    cin >> n;
+    int a[n];
+    for ( int i = 0 ; i < n ; i++ ) {
+        cin >> a[i];
+    }
+
+    mergesort(a,0,n-1);
+    for ( int i = 0 ; i < n ; i++ ) {
+        cout << a[i] << " ";
+    }
+}
